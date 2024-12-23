e9261004f6d48f2e468af119d3d7ba4b42baf33d
Jake Soenneker <jake@soeneker.com>
2024-12-23 22:04:51 +0000

    Tests

--- test/Soenneker.Html.Parser.Tests/HtmlParserUtilTests.cs
+++ test/Soenneker.Html.Parser.Tests/HtmlParserUtilTests.cs
@@ -2,7 +2,6 @@ using Soenneker.Html.Parser.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
-
 namespace Soenneker.Html.Parser.Tests;
 
 [Collection("Collection")]
@@ -13,5 +12,11 @@ public class HtmlParserUtilTests : FixturedUnitTest
     public HtmlParserUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
         _util = Resolve<IHtmlParserUtil>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
     }
 }
