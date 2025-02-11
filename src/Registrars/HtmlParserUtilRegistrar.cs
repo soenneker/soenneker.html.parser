8ed3f1578347a959a34fe070087e1a82129fd3c1
Jake Soenneker <jake@soenneker.com>
2025-02-11 12:54:47 -0600

    Fluent

--- src/Registrars/HtmlParserUtilRegistrar.cs
+++ src/Registrars/HtmlParserUtilRegistrar.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.Html.Client.Registrars;
 using Soenneker.Html.Parser.Abstract;
@@ -13,18 +13,20 @@ public static class HtmlParserUtilRegistrar
     /// <summary>
     /// Adds <see cref="IHtmlParserUtil"/> as a singleton service. <para/>
     /// </summary>
-    public static void AddHtmlParserUtilAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddHtmlParserUtilAsSingleton(this IServiceCollection services)
     {
         services.AddHtmlClientAsSingleton();
         services.TryAddSingleton<IHtmlParserUtil, HtmlParserUtil>();
+        return services;
     }
 
     /// <summary>
     /// Adds <see cref="IHtmlParserUtil"/> as a scoped service. <para/>
     /// </summary>
-    public static void AddHtmlParserUtilAsScoped(this IServiceCollection services)
+    public static IServiceCollection AddHtmlParserUtilAsScoped(this IServiceCollection services)
     {
         services.AddHtmlClientAsScoped();
         services.TryAddScoped<IHtmlParserUtil, HtmlParserUtil>();
+        return services;
     }
 }
